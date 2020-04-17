20bdeee50508f9e798f15c8cb3823a917f334cad
Orlando3011 <34520084+Orlando3011@users.noreply.github.com>
2020-04-17 12:38:15 +0200

    Initial commit

---
+++ src/main/java/pl/psk/bacteriaSimulator/BacteriaSimulatorApplication.java
@@ -0,0 +1,13 @@
+package pl.psk.bacteriaSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacteriaSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacteriaSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/psk/bacteriaSimulator/BacteriaSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.psk.bacteriaSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacteriaSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
